fcbf309546e3cfbf8413934ee7a09bc4a47c55a2
xsimonl00 <xsimonl00@stud.fit.vutbr.cz>
2025-02-26 11:50:13 +0100

    feat: args parsing

---
+++ ArgsOptions.cs
@@ -0,0 +1,22 @@
+using CommandLine;
+
+namespace proj1
+{
+    class ArgsOptions
+    {
+        [Option('i', "interface", Required = false, HelpText = "Specify network interface (e.g., eth0). If omitted, lists active interfaces.")]
+        public string? Interface { get; set; }
+
+        [Option('u', "udp-ports", Required = false, HelpText = "UDP ports to scan (e.g., 53,67 or 1-65535).")]
+        public string? UdpPorts { get; set; }
+
+        [Option('t', "tcp-ports", Required = false, HelpText = "TCP ports to scan (e.g., 22,23,24 or 1-65535).")]
+        public string? TcpPorts { get; set; }
+
+        [Option('w', "wait", Required = false, Default = 5000, HelpText = "Timeout in milliseconds for each port scan.")]
+        public int? Timeout { get; set; }
+
+        [Value(0, MetaName = "target", Required = false, HelpText = "Target domain name or IP address to scan.")]
+        public string? Target { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CommandLine;
+
+namespace proj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<ArgsOptions>(args)
+            .WithParsed(options =>
+            {
+                if (string.IsNullOrEmpty(options.Interface) && string.IsNullOrEmpty(options.Target))
+                {
+                    Console.WriteLine("Listing active interfaces...");
+                    
+                    return;
+                }
+
+                Console.WriteLine($"Interface: {options.Interface ?? "None"}");
+                Console.WriteLine($"TCP Ports: {options.TcpPorts ?? "None"}");
+                Console.WriteLine($"UDP Ports: {options.UdpPorts ?? "None"}");
+                Console.WriteLine($"Timeout: {options.Timeout} ms");
+                Console.WriteLine($"Target: {options.Target ?? "None"}");
+
+                
+            })
+            .WithNotParsed(errors =>
+            {
+                Console.WriteLine("Invalid arguments provided.");
+                foreach (var error in errors)
+                {
+                    Console.WriteLine(error.ToString());
+                }
+            });
+        }
+    }
+}
