fe2c64bc31f74256a64ac34175eb1178db33511b
xsimonl00 <xsimonl00@stud.fit.vutbr.cz>
2025-03-24 21:22:34 +0100

    feat: edit test script

--- test.sh
+++ test.sh
@@ -4,8 +4,8 @@
 tcpPorts="21,80"
 target="2a00:1450:4014:80f::200e"
 
-# Run your program and save the output
-dotnet run --interface tun0 -t "$tcpPorts" -w 3000 "$target" > my_scan_output_raw.txt
+# Run your program and save the output, while also displaying it in real-time
+(dotnet run --interface tun0 -t "$tcpPorts" -w 1200 "$target" | tee my_scan_output_raw.txt) 2>&1
 
 # Run Nmap and format its output similarly to your program
 nmap -6 -p "$tcpPorts" "$target" | awk '/^[0-9]+\/(tcp|udp)/ {split($1, a, "/"); print "'$target'", a[1], a[2], $2}' > nmap_output.txt
@@ -27,3 +27,7 @@ else
     # If no differences, print in green
     echo -e "\033[0;32mOK - The outputs match!\033[0m"
 fi
+
+# Display original Nmap output
+echo -e "\n\033[0;34mOriginal Nmap Output:\033[0m"
+cat original_nmap_output.txt
